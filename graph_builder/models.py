a8f06992574a6549e20c9dfcd4a82826a6537e00
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-06 11:38:05 -0700

    defined Character, Char_Deff, Word, and Word_Deff models. Also updated mysite/settings.py INSTALLED_APPS to include graph_builder app

--- graph_builder/models.py
+++ graph_builder/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    symbol = models.CharField(max_length = 1)
+    rank = models.IntegerField(default = 0)
+
+
+#definition formating to be decided
+class Char_Deff(models.Model):
+    character = models.ForeignKey(Character, on_delete = models.CASCADE)
+    definition = models.CharField(max_length = 1000)
+    pronunciation = models.CharField(max_length = 40)
+    #ordinal ranks deffintions from first to last
+    ordinal = models.IntegerField(default = 0)
+
+
+class Word(models.Model):
+    text = models.CharField(max_length=3)
+    definition = models.CharField(max_length = 1000)
+    rank = models.IntegerField(default = 0)
+
+
+class Word_Deff(models.Model):
+    word = models.ForeignKey(Word, on_delete = models.CASCADE)
+    definition = models.CharField(max_length = 1000)
+    pronunciation = models.CharField(max_length = 40)
+    # ordinal ranks deffintions from first to last
+    ordinal = models.IntegerField(default = 0)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'graph_builder.apps.GraphBuilderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
