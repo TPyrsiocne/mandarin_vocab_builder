6308a5b12e5af0553546f75b1964050794625ca9
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-08 07:40:55 -0700

    creatred scripts folder and preliminary version of populate_tables.py which loads database with characters and character definitions from the char_dict.txt file

---
+++ scripts/populate_tables.py
@@ -0,0 +1,30 @@
+from graph_builder.models import Character, Word, Char_Deff, Word_Deff
+
+# this probably dosen't work the way I think it does. It might depend on the location where script is run
+PATH_TO_CHARDICT = "../char_dict.txt"
+
+
+#clear dictionary of characters and their deffinitions
+Character.objects.all().delete()
+Char_Deff.objects.all().delete()
+
+
+
+for line in open(PATH_TO_CHARDICT, "r").readlines()[:3000]:
+    if line[0].isnumeric():
+        tokens = line.split("\t")
+        """
+        token[0] == rank
+        token[1] == char
+        token[2] == individual raw frequency
+        token[3] == cumulative frequency in precentile
+        token[4] == pinyin(s)
+        token[5] == english translation(s)
+        
+        all tokens are strings, mutiple deffinitions and pronuncations are deliomited by '/'. 
+        """
+        new_char = Character(symbol = tokens[1], rank = tokens[0])
+        new_char_deff = Char_Deff(character = new_char, definition= tokens[5], pronunciation= tokens[4], ordinal = 1)
+        new_char.save()
+        new_char_deff.save()
+
