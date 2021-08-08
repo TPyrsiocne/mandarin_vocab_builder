e80e9ab80eafc77d2f9639150dec1f07b313f8a9
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-08 11:39:17 -0700

    populate_tables.py now populats words

--- scripts/populate_tables.py
+++ scripts/populate_tables.py
@@ -6,6 +6,7 @@ PATH_TOCHARDICT was being interpereted relative to wherever
 command was run. fixed this by including entire file path
 """
 PATH_TO_CHARDICT = "/Users/tysonprice/mandarin_vocab_builder/char_dict.txt"
+PATH_TO_WORDDICT = "/Users/tysonprice/mandarin_vocab_builder/word_dict.txt"
 
 
 #clear dictionary of characters and their deffinitions
@@ -30,4 +31,22 @@ for line in open(PATH_TO_CHARDICT, "r").readlines()[:3000]:
         new_char_deff = Char_Deff(character = new_char, definition= tokens[5], pronunciation= tokens[4], ordinal = 1)
         new_char.save()
         new_char_deff.save()
+
+Word.objects.all().delete()
+Word_Deff.objects.all().delete()
+
+for line in open(PATH_TO_WORDDICT, "r").readlines()[:3000]:
+    if line[0].isnumeric():
+        tokens = line.split("\t")
+        """
+        token[0] == rank
+        token[1] == symbols
+        token[2] == ?
+        token[3] == ?
+        token[4] == ?
+
+        all tokens are strings. 
+        """
+        new_word = Word(symbols=tokens[1], rank=int(tokens[0]))
+        new_word.save()
 
