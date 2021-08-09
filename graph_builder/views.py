7e67326469af2060e11378ff227a57c087ed31f2
Tyson Price <tysonprice@Tysons-MacBook-Pro.local>
2021-08-09 12:52:22 -0700

    modified view to only desplay links to characters with lesser or equal rank

--- graph_builder/views.py
+++ graph_builder/views.py
@@ -5,7 +5,8 @@ def index(request):
     return HttpResponse("Hello, world. You're at the index.")
 
 def graph_build(request, char):
-    char_deff = Char_Deff.objects.get(character__symbol = char)
+    this_char = Character.objects.get(symbol = char)
+    this_char_deff = Char_Deff.objects.get(character__symbol = char)
     """
       how do I sort QuerySets bassed on the other chars' rank?
       how to filter words by rank
@@ -13,11 +14,29 @@ def graph_build(request, char):
     char_initial_words = Word.objects.filter(symbols__startswith = char)
     char_final_words = Word.objects.filter(symbols__endswith = char)
 
+
+    """
+    there might be a better way to do this using filter
+    """
+    rank_limit = this_char.rank
+    word_starters = []
+    for word in char_final_words:
+        starter = Character.objects.get(symbol=word.symbols[0])
+        if starter.rank <= rank_limit:
+            word_starters.append(starter)
+
+    word_enders = []
+    for word in char_initial_words:
+        ender = Character.objects.get(symbol=word.symbols[1])
+        if ender.rank <= rank_limit:
+            word_enders.append(ender)
+
+
     context = {
-        'char' : char,
-        'char_deff' : char_deff,
-        'char_initial_words' : char_initial_words,
-        'char_final_words' : char_final_words,
+        'this_char' : this_char,
+        'this_char_deff' : this_char_deff,
+        'word_starters' : word_starters,
+        'word_enders' : word_enders,
         }
     return render(request, "graph_builder/graph_build.html", context)
 
